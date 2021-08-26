33a58951d78974eb19d7f96db2687eaf4035fca0
Andrielson Silva <andrielson.silva@capco.com>
2021-08-26 15:49:24 -0300

    Project created with Spring Initializr

---
+++ src/main/java/io/github/andrielson/spring_boot_forum/SpringBootForumApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.andrielson.spring_boot_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/andrielson/spring_boot_forum/SpringBootForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrielson.spring_boot_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
