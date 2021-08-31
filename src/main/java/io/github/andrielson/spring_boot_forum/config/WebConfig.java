2021ca18bf5a8a19353f35f3012d4e04bc08b9a3
Andrielson Silva <andrielson@gmail.com>
2021-08-30 22:40:37 -0300

    Fix CORS configuration for Swagger UI

--- src/main/java/io/github/andrielson/spring_boot_forum/config/WebConfig.java
+++ src/main/java/io/github/andrielson/spring_boot_forum/config/WebConfig.java
@@ -3,6 +3,7 @@ package io.github.andrielson.spring_boot_forum.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
@@ -16,6 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600L)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
-                .allowCredentials(true);
+                .allowCredentials(false);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars");
     }
 }
